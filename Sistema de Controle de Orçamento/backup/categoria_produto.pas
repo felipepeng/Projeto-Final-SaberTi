d5a3f86fbe4d510840b7d85cabaed64b9647d59c
Peng <felipebarretocortes.fb@gmail.com>
2025-10-20 16:07:11 -0300

    CatProduto Finalizado

--- Sistema de Controle de Orçamento/backup/categoria_produto.pas
+++ Sistema de Controle de Orçamento/backup/categoria_produto.pas
@@ -25,6 +25,12 @@ type
     qryCatProdutocategoriaprodutoid: TZIntegerField;
     qryCatProdutods_categoria_produto: TZRawStringField;
     updtCatProduto: TZUpdateSQL;
+    procedure btnCancelarClick(Sender: TObject);
+    procedure btnEditarClick(Sender: TObject);
+    procedure btnExcluirClick(Sender: TObject);
+    procedure btnGravarClick(Sender: TObject);
+    procedure btnInserirClick(Sender: TObject);
+    procedure btnPesquisaClick(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormShow(Sender: TObject);
     procedure PanelCadastroCenterClick(Sender: TObject);
@@ -59,6 +65,63 @@ procedure TCategoria_ProdutoF.FormClose(Sender: TObject;
 begin
   inherited;
   qryCatProduto.Close;
+end;
+
+procedure TCategoria_ProdutoF.btnInserirClick(Sender: TObject);
+begin
+  inherited; //Vai para Cadastro
+  //Insert
+  qryCatProduto.Insert;
+end;
+
+procedure TCategoria_ProdutoF.btnPesquisaClick(Sender: TObject);
+begin
+  //Fecha a Query
+  qryCatProduto.Close;
+
+  //Edita o comando SQL
+  if edtPesquisa.Text <> '' then
+  begin
+    qryCatProduto.SQL.Text:= ('select * from categoria_produto' +
+                              ' where categoriaprodutoid =' + edtPesquisa.Text);
+  end else
+  begin
+    qryCatProduto.SQL.Text:= ('select * from categoria_produto;');
+  end;
+
+  //Reabre a Query
+  qryCatProduto.Open;
+end;
+
+procedure TCategoria_ProdutoF.btnGravarClick(Sender: TObject);
+begin
+  inherited; //Vai para Consulta
+  //Confirma o Insert
+  qryCatProduto.Post;
+end;
+
+procedure TCategoria_ProdutoF.btnEditarClick(Sender: TObject);
+begin
+  inherited; //Faz Nada
+  //Edit
+  qryCatProduto.Edit;
+end;
+
+procedure TCategoria_ProdutoF.btnExcluirClick(Sender: TObject);
+begin
+  //Exclui
+   If  MessageDlg('Atenção', 'Você tem certeza que deseja excluir o registro?', mtConfirmation,[mbyes,mbno],0) = mryes then
+  begin
+    qryCatProduto.Delete;
+    inherited; //Vai para Consulta
+  end;
+end;
+
+procedure TCategoria_ProdutoF.btnCancelarClick(Sender: TObject);
+begin
+  inherited; //Vai para Consulta
+  //Cancela
+  qryCatProduto.Cancel;
 end;
 
 end.
